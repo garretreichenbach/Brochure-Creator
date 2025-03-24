450bd11d0e94a5bc3303027e310065f48256095b
Garret Reichenbach <garretreichenbach@gmail.com>
2025-03-24 11:41:22 -0700

    Website updates.

--- docs/assets/js/generator/generator.mjs
+++ docs/assets/js/generator/generator.mjs
@@ -1,1 +1,1 @@
-import t,{useState as i}from"https://unpkg.com/react@18/umd/react.production.min.js";import m from"https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
+import{useState as r}from"react";
